0fbec19a90c411f2bb981da542f20a8fb84892d8
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-01-07 18:11:44 -0500

    add variables.js

---
+++ Syntax/variables.js
@@ -0,0 +1,28 @@
+
+// var 
+console.log('~~~~~ var ~~~~~');
+
+var someThing;
+console.log(someThing);
+var someThing = 5;
+console.log(someThing);
+someThing = "Bob";
+console.log(someThing);
+
+// let
+console.log('~~~~~ let ~~~~~');
+
+let price;
+console.log(price);
+price = 10.00;
+console.log(price);
+price = 40.00;
+console.log(price);
+
+// const
+console.log('~~~~~ const ~~~~~');
+
+// const must be assigned a value and cannot be changed
+const constantValue = 10;
+console.log(constantValue);
+
